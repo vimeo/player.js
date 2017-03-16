51846c71bdd30de309e62a4a7d8d1026bb18274c
Colin Leong <cleong@tucows.com>
2017-03-16 13:05:08 -0400

    remove unnecessary/extra isNode check

--- src/lib/compatibility-check.js
+++ src/lib/compatibility-check.js
@@ -3,7 +3,7 @@ const arrayIndexOfSupport = typeof Array.prototype.indexOf !== 'undefined';
 const postMessageSupport = typeof window.postMessage !== 'undefined';
 
 export function checkCompatibility() {
-    if (!isNode && (!arrayIndexOfSupport || !postMessageSupport)) {
+    if (!arrayIndexOfSupport || !postMessageSupport) {
         throw new Error('Sorry, the Vimeo Player API is not available in this browser.');
     }
 }
