254a87046fa9bd065d11cc1cb28e0f490da3b911
Colin Leong <cleong@tucows.com>
2017-04-24 10:57:41 -0400

    add window-undefined-check for postMessageSupport as suggested by kavsingh

--- src/lib/compatibility-check.js
+++ src/lib/compatibility-check.js
@@ -1,6 +1,6 @@
 export const isNode = typeof global !== 'undefined' && ({}).toString.call(global) === '[object global]';
 const arrayIndexOfSupport = typeof Array.prototype.indexOf !== 'undefined';
-const postMessageSupport = typeof window.postMessage !== 'undefined';
+const postMessageSupport = typeof window !== 'undefined' && typeof window.postMessage !== 'undefined';
 
 export function checkCompatibility() {
     if (!arrayIndexOfSupport || !postMessageSupport) {
