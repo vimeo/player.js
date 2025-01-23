a4d4939d495d1496e8de1bfa79be1e34157277c5
Henry <mcintyrehh@gmail.com>
2025-01-23 15:49:57 -0500

    v2.25.1

--- dist/player.js
+++ dist/player.js
@@ -1,4 +1,4 @@
-/*! @vimeo/player v2.25.0 | (c) 2024 Vimeo | MIT License | https://github.com/vimeo/player.js */
+/*! @vimeo/player v2.25.1 | (c) 2025 Vimeo | MIT License | https://github.com/vimeo/player.js */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
