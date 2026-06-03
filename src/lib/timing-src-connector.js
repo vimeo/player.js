b2576fc560e2355cd5fd97556d6a86c9001abc58
Alex Paseltiner <aspaseltiner+vimeo@gmail.com>
2026-06-03 08:47:46 -0400

    TimingSrcConnector: force pause after fallback attempt at muted autoplay fails

--- src/lib/timing-src-connector.js
+++ src/lib/timing-src-connector.js
@@ -130,7 +130,10 @@ export class TimingSrcConnector extends EventTarget {
                         .catch(async(err) => {
                             if (err.name === 'NotAllowedError' && options.autoPlayMuted) {
                                 await player.setMuted(true);
-                                await player.play().catch((err2) => console.error('Couldn\'t play the video from TimingSrcConnector. Error:', err2));
+                                await player.play().catch(async err2 => {
+                                    console.error('Couldn\'t play the video from TimingSrcConnector. Error:', err2);
+                                    await player.pause();
+                                });
                             }
                         });
                     this.updatePlayer(timingObject, player, options);
